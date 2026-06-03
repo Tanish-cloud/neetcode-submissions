e184d4c1f85fbd5f1637e31a459e2e14e61baa7a
Tanish-cloud <139438686+Tanish-cloud@users.noreply.github.com>
2026-06-03 10:55:50 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            diff = target - nums[i]
+
+            if diff in nums:
+                j = nums.index(diff)
+
+                if i < j:
+                    return [i, j]
+                elif i > j:
+                    return [j, i]
