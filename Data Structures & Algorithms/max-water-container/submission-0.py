02dee7ae727e57cbb21c72b023a90a5d595e1c6a
Tanish-cloud <139438686+Tanish-cloud@users.noreply.github.com>
2026-06-17 15:10:13 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        left,right=0,len(heights)-1
+        max_water=0
+        while left<right:
+            h=min(heights[left],heights[right])
+            w=right-left
+            max_water=max(max_water,h*w)
+            if heights[left]<heights[right]:
+                left+=1
+            else:
+                right-=1
+        return max_water
